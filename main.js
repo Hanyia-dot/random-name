9cc128d1f1f49d4e41fa43a09294ac6fc9b84753
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-05 20:22:20 +0200

    add event listener in main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector('button');
+const div = document.querySelector('div');
+
+const names = ["chybasilny", "seba_stanek", "pan_michau", "pan_filip_", "strongvet_", "dlmstr_patrick", "miiciiu", "piternoof", "no_i_pan_grzegorz", "zurekkous", "_olej_to", "mr.grecku", "pan_arnold", "normalny.fajny.chlopak.culejto", "chlorton", "adam.chudas", "szymokoks", "sebo_96", "b.bigaj"];
+
+const randomName = () => {
+    //div.textContent = `Wygrywa: ${names[10]}`;
+
+}
+
+
+
+document.addEventListener('click', randomName);
