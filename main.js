00dc58389af41064cdd7e5c18fbc27cc721fd1e5
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-06-05 21:24:36 +0200

    add code to main.js

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 const btn = document.querySelector('button');
 const div = document.querySelector('div');
+const img = document.querySelector('img');
 
 const names = ["chybasilny", "seba_stanek", "pan_michau", "pan_filip_", "strongvet_", "dlmstr_patrick", "miiciiu", "piternoof", "no_i_pan_grzegorz", "zurekkous", "_olej_to", "mr.grecku", "pan_arnold", "normalny.fajny.chlopak.culejto", "chlorton", "adam.chudas", "szymokoks", "sebo_96", "b.bigaj"];
 
-const randomName = () => {
-    //div.textContent = `Wygrywa: ${names[10]}`;
+
+const nameGenerator = () => {
+    const randomNumber = Math.floor(Math.random() * names.length);
+    div.textContent = `Najprawdopodobniej najlepszym lifterem jest ${names[randomNumber]}. Gratulujemy!`;
 
 }
 
-
-
-document.addEventListener('click', randomName);
+document.addEventListener('click', nameGenerator);
